aa0e2c81c0bd28a0e6ba2a508931831d7e647f7d
Mohamed Ashif K M <mohamedashif1911@gmail.com>
2024-03-23 19:31:30 +0530

    connected atlas

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,21 @@ const userModel = require('./config')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin: ("http://localhost:3000"),
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+))
 
-mongoose.connect("mongodb://localhost:27017/Jarvis");
+mongoose.connect("mongodb+srv://Ashif-107:Ashi@cluster0.qhzatuq.mongodb.net/Jarvis?retryWrites=true&w=majority&appName=Cluster0")
+        .then(() => console.log("Connected to MongoDB"))
+        .catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
 app.post('/signup', (req, res) => {
     userModel.create(req.body).then(users => res.json(users)).catch(err => res.json(err))
@@ -33,6 +45,6 @@ app.post('/login', (req, res) => {
 
 })
 
-    app.listen(3001, () => {
-        console.log("Server is running on port 3001")
-    })
+app.listen(3001, () => {
+    console.log("Server is running on port 3001")
+})
